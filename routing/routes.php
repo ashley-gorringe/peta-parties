b5cf20531b6080a30cdfba1a9a1f4325cab02d3d
Ashley Gorringe <ashley@splycd.co.uk>
2022-01-11 09:35:34 +0000

    Updated links.

--- routing/routes.php
+++ routing/routes.php
@@ -26,5 +26,24 @@ Route::add('/shop/([0-9a-zA-Z]*)', function($id) {
 	require_once dirname($_SERVER['DOCUMENT_ROOT']).'/routing/product.php';
 });
 
+Route::add('/about', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+Route::add('/contact', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+Route::add('/customer-services', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+Route::add('/returns', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+Route::add('/terms', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+Route::add('/privacy', function() {
+	echo $GLOBALS['twig']->render('coming-soon.twig');
+});
+
 Route::run('/');
 ?>
